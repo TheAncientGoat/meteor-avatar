d7e01e19cf8b79733ad467cec4a374847fe0c171
Ryan Swart <ryanswrt@gmail.com>
2016-02-27 14:28:19 +1100

    update stylus dep

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "ryanswrt:avatar",
   summary: "Consolidated user avatar template (twitter, facebook, gravatar, etc.) - Forked from bengott",
-  version: "0.7.8",
+  version: "0.7.10",
   git: "https://github.com/TheAncientGoat/meteor-avatar"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom(['METEOR@0.9.4.1', 'METEOR@1.1.0.1']);
+  //api.versionsFrom(['METEOR@0.9.4.1', 'METEOR@1.1.0.1']);
   api.use(['templating', 'stylus', 'reactive-var'], ['client']);
   api.use(['underscore', 'jparker:gravatar@0.3.1'], ['client', 'server']);
   api.addFiles(
@@ -17,11 +17,14 @@ Package.onUse(function(api) {
     ],
     ['client']
   );
+
+    api.addAssets([
+        'default.png'
+    ], ['client','server']);
   api.addFiles(
     [
       'export.js',
-      'helpers.js',
-      'default.png'
+      'helpers.js'
     ],
     ['client', 'server']
   );
